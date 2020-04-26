8bb8050736c1fa387dfd73ae04d8df3a9bf0bb80
Szabó Attila <tailor993@gmail.com>
2020-04-26 09:54:29 +0200

    Initial

---
+++ Ellenorzo/Jegy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ellenorzo
+{
+    class Jegy
+    {
+        int erdemjegy;
+
+        public Jegy(int jegy)
+        {
+            Erdemjegy = jegy;
+        }
+
+        public int Erdemjegy { get => erdemjegy; set => erdemjegy = value; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 8-ellenorzo-alkalmazas-onallo-feladat
+
+# 8. Óra - Ellenőrző alkalmazás készítése - Önálló feladat
+
+## Ismétlés
+- Osztályok
+- Példányosítás
+- Listbox használata
+- Combobox használata
+- Getter és Setterek használata
+- Listák használata
